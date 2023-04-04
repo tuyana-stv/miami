d5a411af7cf465a6d9648787ce90b8aee35e88e8
tuyana-stv <tuyana240696@gmail.com>
2023-04-04 15:56:50 +0300

    Add burger menu

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,42 @@
-$(document).ready(function() {
-	$('.header__icon-burger').click(function(){
-        $('.header__icon-burger').toggleClass('open-menu');
-        $('.nav').toggleClass('open-menu');
-	});
+const burgerMenu = document.querySelector('.header__menu');
+const burgerMenuOpen = document.querySelector('.burger-menu__open');
+const burgerMenuClose = document.querySelector('.menu-header__icon-close');
+const navLinks = document.querySelectorAll('a[href*="#"]');
+const body = document.querySelector('body');
+
+const onDocumentKeydown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeBurgerMenu();
+  }
+};
+
+function openBurgerMenu() {
+  burgerMenu.classList.add('active');
+  body.classList.add('fixed-page');
+  document.addEventListener('keydown', onDocumentKeydown);
+}
+
+function closeBurgerMenu() {
+  burgerMenu.classList.remove('active');
+  body.classList.remove('fixed-page');
+  document.removeEventListener('keydown', onDocumentKeydown);
+}
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', closeBurgerMenu);
+  link.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    const id = link.getAttribute('href').substr(1);
+      
+    document.getElementById(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
 });
+
+burgerMenuOpen.addEventListener('click', openBurgerMenu);
+
+burgerMenuClose.addEventListener('click', closeBurgerMenu);
+
